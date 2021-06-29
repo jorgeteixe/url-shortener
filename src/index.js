203b78c2dd76cc0345046c6d79be89788f10cc88
Jorge Teixeira <jorge.teixeira@bizaway.com>
2021-06-29 21:30:34 +0200

    response for redirect to 302 and exit on mongo error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .catch(e => {
-    console.error('Coundn\'t connect to mongo', e)
+    console.error('Coundn\'t connect to mongo:\n', e)
+    process.exit(1)
   })
 
 const app = express()
